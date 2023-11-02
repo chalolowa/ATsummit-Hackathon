7f798765db709dfc98f224326505c6c65b315d59
chalolowa <wangwelocha@students.uonbi.ac.ke>
2023-11-02 18:38:08 +0300

    fixed the websocket connection

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const http = require("http");
-const ws = require("ws");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const csrf = require("csurf");
@@ -12,9 +11,8 @@ let csrfProtection = csrf({ cookie: true });
 let parseForm = express.urlencoded({ extended: false });
 
 //initialize express
-const server = express();
-const connect = http.createServer(server);
-const wss = new ws.Server({ server });
+const app = express();
+const server = http.createServer();
 
 //initilize admin
 admin.initializeApp({
@@ -33,71 +31,37 @@ const firebaseConfig = {
   appId: "1:764239832937:web:7bf322f58ad65cf369bff8",
   measurementId: "G-NVWVESQDSG",
 };
-
 firebase.initializeApp(firebaseConfig);
 
-server.post("/", function (req, res) {
-  const query = req.body.cityName;
-  const apiKey = "2ba3f323809895bb010d33f8d63f037f";
-  const unit = "metric";
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query +
-    "&appid=2ba3f323809895bb010d33f8d63f037f";
-  http.get(url, function (response) {
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      console.log(temp);
-      const weatherDescrption = weatherData.weather[0].description;
-      console.log(weatherDescrption);
-      var temperatureInDegree = Math.floor(temp - 273);
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write(
-        "<h1>The  temperature in " +
-          query +
-          " is " +
-          temperatureInDegree +
-          " degrees celsiures</h1>"
-      );
-      res.write("<p>The weather is " + weatherDescrption + " </p>");
-      res.write("<img src='" + imageURL + "'>");
-
-      res.send();
-    });
-  });
-});
-
 //render static files
-server.use(express.static("public"));
-server.use("/css", express.static(__dirname + "/public/css"));
-server.use("/images", express.static(__dirname + "/public/images"));
-server.use("/js", express.static(__dirname + "/public/js"));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/images", express.static(__dirname + "/public/images"));
+app.use("/js", express.static(__dirname + "/public/js"));
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Set the view engine to ejs
-server.set("view engine", "ejs");
-server.set("views", "./views");
-server.get('/', (req, res) => {
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.get('/', (req, res) => {
   res.render('homepage');
 });
 
 //crsf protection
-server.get("/form", csrfProtection, function (req, res) {
+app.get("/form", csrfProtection, function (req, res) {
   res.render("send", { csrfToken: req.csrfToken() });
 });
-server.post("/process", parseForm, csrfProtection, function (req, res) {
+app.post("/process", parseForm, csrfProtection, function (req, res) {
   res.send("data is being processed");
 });
 
-server.get("/login", (req, res) => {
+app.get("/login", (req, res) => {
   res.render("login");
 });
-server.post('/login', async (req, res) => {
+app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
@@ -109,10 +73,10 @@ server.post('/login', async (req, res) => {
   }
 });
 
-server.get("/signup", (req, res) => {
+app.get("/signup", (req, res) => {
   res.render("signup");
 });
-server.post('/signup', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, username } = req.body;
   try {
     // Create user in Firebase Authentication
@@ -133,20 +97,33 @@ server.post('/signup', async (req, res) => {
   }
 });
 
-wss.on('connection', (ws) => {
-  console.log('WebSocket connection established');
-
-  ws.on('message', (message) => {
-    console.log('Received message:', message);
-  });
-
-  ws.on('close', () => {
-    console.log('WebSocket connection closed');
-  });
-});
-
 // Start the Express server
+server.on('request', app)
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log('Clients connected', numClients);
+
+  wss.broadcast('Current visitors: ${numClients}');
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('Welcome');
+  }
+
+  ws.on('close', function close() {
+    console.log('Client disconnected');
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+}
